121df53eacb43c5a385a8ea5edc86622c9dec912
Akkarim <luis.carvajalrojas@ucr.ac.cr>
2018-09-20 16:17:54 -0600

    Persona listo... Creo

--- ProyectoSimuladorVirus/ClasePersona.cpp
+++ ProyectoSimuladorVirus/ClasePersona.cpp
@@ -1,15 +1,38 @@
 #include "ClasePersona.h"
 
-
-
 ClasePersona::ClasePersona()
 {
 }
-
-
 ClasePersona::~ClasePersona()
 {
 }
+
+void ClasePersona::setEstado(int status) {
+	this->estado = status;
+}
+
+int ClasePersona::getEstado() {
+	return this->estado;
+}
+
+void ClasePersona::setPosicion(pair<int, int> pos) {
+	this->posicion = pos;
+}
+
+pair<int, int> ClasePersona::getPosicion() {
+	return this->posicion;
+}
+
+void ClasePersona::setProbaInf(double inf){
+	this->probaInfectibilidad = inf;
+}
+
+void ClasePersona::setProbaRec(double rec) {
+	this->probaRecuperacion = rec;
+}
+
+
+
 
 //End of file with a Cow (Bettsy)
 //                               __.----.___
--- ProyectoSimuladorVirus/ClasePersona.h
+++ ProyectoSimuladorVirus/ClasePersona.h
@@ -1,10 +1,37 @@
 #pragma once
+#include <utility>
+using namespace std;
+
 class ClasePersona
 {
 public:
+	double probaInfectibilidad;
+	double probaRecuperacion;
+	int estado;
+	pair<int, int> posicion;
+
 	ClasePersona();
 	~ClasePersona();
+
+	//EFE: Set de recuperación e infección
+	void setProbaInf(double inf);
+	void setProbaRec(double rec);
+	/*
+	0-Sano
+	1-Enfermo
+	2-Inmune
+	3-did
+	*/
+	void setEstado(int status);
+	int getEstado();
+
+	//EFE: Retorna posicion
+	void setPosicion(pair<int, int>);
+	pair<int, int> getPosicion();
+
 };
+
+
 
 //End of file with a Cow (Bettsy)
 //                               __.----.___
